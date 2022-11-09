ac1a009ab167ed6df3fe240b8adedfb3c4dbf66c
Josh Attenberg <josh.attenberg@gmail.com>
2022-11-09 12:58:31 -0500

    initial build

---
+++ build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# i use this to build a useful virtualenv for local development.
+# i use anaconda as a base (i think with py 3.8 at the moment)
+
+set -e
+
+if ! command -v virtualenv &> /dev/null
+then
+    echo "virtualenv could not be found"
+    pip install virtualenv;
+else
+  venv_path=$(which virtualenv);
+  echo "using venv located at $venv_path"
+fi
+
+venv="venv"
+echo "making virtualenv: $venv"
+
+virtualenv $venv
+
+$venv/bin/pip install --upgrade pip pytest
+$venv/bin/pip install pip-tools
+$venv/bin/pip install -r requirements.txt
+$venv/bin/jupyter nbextension install https://github.com/drillan/jupyter-black/archive/master.zip
+$venv/bin/jupyter nbextension enable jupyter-black-master/jupyter-black
